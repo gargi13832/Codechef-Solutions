69db950fe87917ee37b200ba81327537131534e7
gargi13832 <gargi.kadam17@vit.edu>
2020-09-01 19:46:01 +0530

    small-remainder

---
+++ Beginner/small-remainder.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int test;
+    cin>>test;
+    int i,a,b;
+    for(i=0;i<test;i++)
+    {
+        cin>>a>>b;
+        cout<<a%b<<endl;
+    }
+    return 0;
+}
+
