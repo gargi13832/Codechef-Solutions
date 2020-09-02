8722d51403a85cdda4e1c59b8a4737ae13464232
gargi13832 <gargi.kadam17@vit.edu>
2020-09-02 19:27:04 +0530

    packing-cupcakes

---
+++ Beginner/packing-cupcakes.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,test;
+    cin>>test;
+    while(test--)
+    {
+        cin>>n;
+        cout<<(n/2)+1<<endl;
+    }
+    return 0;
+}
+
