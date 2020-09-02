a3ee844acb8cb6d5211216b7fcb50ce284668b0a
gargi13832 <gargi.kadam17@vit.edu>
2020-09-02 19:24:26 +0530

    finding-the-square-roots

---
+++ Beginner/finding-the-square-roots.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    int test;
+    cin>>test;
+    int i,n;
+    for(i=0;i<test;i++)
+    {
+        cin>>n;
+        cout<<(int)sqrt(n)<<endl;
+    }
+    return 0;
+}
+
